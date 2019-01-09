9c59fc302477c6ae3972774816afa8cafab416c2
Felipe Santos <santos.felipe13@gmail.com>
2019-01-09 17:27:49 -0200

    contact form validations

--- app/assets/javascripts/mail-teste.js
+++ app/assets/javascripts/mail-teste.js
@@ -5,10 +5,10 @@ function l(x) {
 }
 
 
-document.querySelector('#contact-nome').value = 'Frederico Gomes Pereira'
-document.querySelector('#contact-email').value = 'fredgomes@gmail.com'
-document.querySelector('#contact-telefone').value = '(31) 99254-6514'
-document.querySelector('#contact-assunto').value = 'Vaga de estagio'
-document.querySelector('#contact-mensagem').value = 'Caros amigos, a revolução dos costumes nos obriga à análise das condições financeiras e administrativas exigidas. As experiências acumuladas demonstram que a expansão dos mercados mundiais possibilita uma melhor visão global das regras de conduta normativas. O empenho em analisar a contínua expansão de nossa atividade exige a precisão e a definição dos conhecimentos estratégicos para atingir a excelência. Por conseguinte, a hegemonia do ambiente político facilita a criação do orçamento setorial. '
+// document.querySelector('#contact-nome').value = 'Frederico Gomes Pereira'
+// document.querySelector('#contact-email').value = 'fredgomes@gmail.com'
+// document.querySelector('#contact-telefone').value = '(31) 99254-6514'
+// document.querySelector('#contact-assunto').value = 'Vaga de estagio'
+// document.querySelector('#contact-mensagem').value = 'Caros amigos, a revolução dos costumes nos obriga à análise das condições financeiras e administrativas exigidas. As experiências acumuladas demonstram que a expansão dos mercados mundiais possibilita uma melhor visão global das regras de conduta normativas. O empenho em analisar a contínua expansão de nossa atividade exige a precisão e a definição dos conhecimentos estratégicos para atingir a excelência. Por conseguinte, a hegemonia do ambiente político facilita a criação do orçamento setorial. '
 
 });
