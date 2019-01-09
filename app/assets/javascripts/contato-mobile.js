f9dc604179c52dcf71f323156d62d76b9af0b609
Felipe Santos <santos.felipe13@gmail.com>
2019-01-09 00:23:12 -0200

    form filling mobile disappear navbar

---
+++ app/assets/javascripts/contato-mobile.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+function l(x) {
+  return console.log(x)
+}
+
+// O objetivo deste script é ocultar a barra de navegação quando o usuário estiver preenchendo
+// o formulario de contato, uma vez que no mobile a navbar em posicao fixed acaba tampando
+// e atrapalhando a digitacao
+
+deviceWidth = window.innerWidth;
+contactFields = document.getElementsByClassName('contact-form-field')
+navbar = document.getElementsByClassName('navbar-hrd')[0]
+
+// On load
+
+Array.prototype.forEach.call(contactFields, function(el) {
+  el.addEventListener('focusout', function() {
+    if (window.innerWidth < 400) {
+      l('oi')
+      navbar.style.display = ''
+    }
+  });
+
+  el.addEventListener('focus', function() {
+    if (window.innerWidth < 400) {
+      l('oi')
+      navbar.style.display = 'none'
+    }
+  });
+});
+
+
+
+});
