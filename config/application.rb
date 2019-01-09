a047a51f991f02ae4c07dd8349c8d0809956c22e
Felipe Santos <santos.felipe13@gmail.com>
2019-01-09 16:15:34 -0200

    postmark settings

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,6 @@ module HrdEngenharia
     config.middleware.use ActionDispatch::Cookies # Required for all session management
     config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
 
-    config.action_mailer.delivery_method = :postmark
-    config.action_mailer.postmark_settings = { :api_token => "888de166-b131-4af8-9498-0dae87c919e2" }
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
