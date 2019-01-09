6589a0f981a5f5591e171f40d57b1b26556cb7f3
Felipe Santos <santos.felipe13@gmail.com>
2019-01-08 23:49:06 -0200

    new swipe logic

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -10,16 +10,6 @@ bannerThirdLine = document.getElementsByClassName("main-title-text-line")[2]
 
 bannerEndY = document.getElementsByClassName("wrapper-page")[0].offsetTop;
 
-// solutionCards = document.getElementsByClassName("solutions-card")
-
-// teamMembersAll = document.getElementsByClassName("team-description-flex")
-// teamMembersFirstRow = []
-// teamMembersFirstRow.push(teamMembersAll[0])
-// teamMembersFirstRow.push(teamMembersAll[1])
-// teamMembersSecondRow = []
-// teamMembersSecondRow.push(teamMembersAll[2])
-// teamMembersSecondRow.push(teamMembersAll[3])
-
 navbarComponent = document.getElementsByClassName("navbar-home")[0]
 
 l(navbarComponent)
@@ -46,32 +36,18 @@ setTimeout(function(){
   }, 300);
 }, 300);
 
-//Scroll solutions scroll-in
-window.addEventListener('scroll', function() {
-  if (window.pageYOffset > 240) {
-    Array.prototype.forEach.call(solutionCards, card => {
-      card.classList.add("come-in");
-    });
-  }
-});
 
-//Scroll team scroll-in first row
-window.addEventListener('scroll', function() {
-  if (window.pageYOffset > 740) {
-    Array.prototype.forEach.call(teamMembersFirstRow, card => {
-      card.classList.add("come-in");
-    });
-  }
-});
+// Cards scroll into view
+// Options
+// Refer to https://scrollrevealjs.org/api/duration.html
+var slideUp = {
+    distance: '70%',
+    origin: 'bottom',
+    opacity: 0.5,
+    duration: 400
+};
 
-//Scroll team scroll-in second row
-window.addEventListener('scroll', function() {
-  if (window.pageYOffset > 980) {
-    Array.prototype.forEach.call(teamMembersSecondRow, card => {
-      card.classList.add("come-in");
-    });
-  }
-});
-
+solutionCards = document.getElementsByClassName("card-solutions-home")
+ScrollReveal().reveal(solutionCards, slideUp);
 
 });
--- app/assets/javascripts/swipe-carousel.js
+++ app/assets/javascripts/swipe-carousel.js
@@ -54,83 +54,89 @@ pageStartY -= document.querySelector('.navbar-hrd').offsetHeight;
 
 l(pageStartY)
 
-// This function  detects swipe movements, and runs function swipeFunction with inputs swiped element
+// This class  detects swipe movements, and runs function swipeFunction with inputs swiped element
 // and swipe direction (u, r, l, d) for up, right, left, down
-function detectswipe(el,func) {
-      swipe_det = new Object();
-      swipe_det.sX = 0;
-      swipe_det.sY = 0;
-      swipe_det.eX = 0;
-      swipe_det.eY = 0;
-      var min_x = 20;  //min x swipe for horizontal swipe
-      var max_x = 40;  //max x difference for vertical swipe
-      var min_y = 40;  //min y swipe for vertical swipe
-      var max_y = 50;  //max y difference for horizontal swipe
-      var direc = "";
-      ele = document.getElementById(el);
-      ele.addEventListener('touchstart',function(e){
-        var t = e.touches[0];
-        swipe_det.sX = t.screenX;
-        swipe_det.sY = t.screenY;
-      },false);
-      ele.addEventListener('touchmove',function(e){
-        e.preventDefault();
-        var t = e.touches[0];
-        swipe_det.eX = t.screenX;
-        swipe_det.eY = t.screenY;
-      },false);
-      ele.addEventListener('touchend',function(e){
-        //horizontal detection
-        if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y)))) {
-          if(swipe_det.eX > swipe_det.sX) direc = "r";
-          else direc = "l";
-        }
-        //vertical detection
-        if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x)))) {
-          if(swipe_det.eY > swipe_det.sY) direc = "d";
-          else direc = "u";
-        }
+// To run a function on given swipe direction, use:
+// (new Swipe('#home-slider')).onLeft(function() { alert('You swiped left.') }).run();
+// In this example, the element with id 'home-slider' will be the canvas
+// If swiped to the left direction, an alert will apear
+class Swipe {
+    constructor(element) {
+        this.xDown = null;
+        this.yDown = null;
+        this.element = typeof(element) === 'string' ? document.querySelector(element) : element;
 
-        if (direc != "") {
-          if(typeof func == 'function') func(el,direc);
-        }
-        direc = "";
-      },false);
+        this.element.addEventListener('touchstart', function(evt) {
+            this.xDown = evt.touches[0].clientX;
+            this.yDown = evt.touches[0].clientY;
+        }.bind(this), false);
+
     }
 
-// Obs: carousel element must be the one with class .carousel
-// This function will cycle slides left and right with user swipe left and right
-// If scroll is down, go to page content smoothly
-function swipeFunction(element,direction) {
-  carousel = document.getElementById('home-slider');
+    onLeft(callback) {
+        this.onLeft = callback;
 
-  slideCount = 0
-  // Get which slide is active
-  Array.prototype.forEach.call(carouselSlides, slide => {
-    if (slide.classList.contains('active')) {
-      activeSlide = slideCount
+        return this;
     }
-    slideCount += 1
-  });
 
-  if (direction === 'l') {
-    l('l')
-    eventFire(carouselRightArrow, 'click');
-  } else if (direction === 'r') {
-    l('r')
-    eventFire(carouselLeftArrow, 'click')
-  } else if (direction === 'u') {
-    doScrolling(pageStartY, 600)
-  }
+    onRight(callback) {
+        this.onRight = callback;
 
-  // carouselSlides[activeSlide].classList.remove('active')
-  // carouselSlides[nextSlide].classList.add('active')
+        return this;
+    }
 
-  // carouselIndicators[activeSlide].classList.remove('active')
-  // carouselIndicators[nextSlide].classList.add('active')
+    onUp(callback) {
+        this.onUp = callback;
+
+        return this;
+    }
+
+    onDown(callback) {
+        this.onDown = callback;
+
+        return this;
+    }
+
+    handleTouchMove(evt) {
+        if ( ! this.xDown || ! this.yDown ) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;
+        var yUp = evt.touches[0].clientY;
+
+        this.xDiff = this.xDown - xUp;
+        this.yDiff = this.yDown - yUp;
+
+        if ( Math.abs( this.xDiff ) > Math.abs( this.yDiff ) ) { // Most significant.
+            if ( this.xDiff > 0 ) {
+                this.onLeft();
+            } else {
+                this.onRight();
+            }
+        } else {
+            if ( this.yDiff > 0 ) {
+                this.onUp();
+            } else {
+                this.onDown();
+            }
+        }
+
+        // Reset values.
+        this.xDown = null;
+        this.yDown = null;
+    }
+
+    run() {
+        this.element.addEventListener('touchmove', function(evt) {
+            this.handleTouchMove(evt).bind(this);
+        }.bind(this), false);
+    }
 }
 
-detectswipe('home-slider',swipeFunction);
+(new Swipe('#home-slider')).onLeft(function() { eventFire(carouselRightArrow, 'click'); }).run();
+(new Swipe('#home-slider')).onRight(function() { eventFire(carouselLeftArrow, 'click'); }).run();
+
 
 });
 
