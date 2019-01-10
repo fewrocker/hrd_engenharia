97804dda7cab6d0e634db2036c1aaa21509c2796
Felipe Santos <santos.felipe13@gmail.com>
2019-01-10 19:23:08 -0200

    fixing navbar on mobile

---
+++ app/assets/javascripts/common.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+function l(x) {
+  return console.log(x)
+}
+
+wrapperSlidershow = document.getElementsByClassName('wrapper-homepage-slideshow')[0]
+screenHeight = window.innerHeight
+
+document.getElementsByClassName('wrapper-homepage-slideshow')[0].style.height = screenHeight = window.innerHeight + 'px'
+
+});
