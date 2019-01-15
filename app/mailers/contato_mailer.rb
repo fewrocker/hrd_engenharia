5ff20b1ed35f438c734a34294ae19756d746aecf
Felipe Santos <santos.felipe13@gmail.com>
2019-01-15 19:24:10 -0200

    final mailer

--- app/mailers/contato_mailer.rb
+++ app/mailers/contato_mailer.rb
@@ -1,6 +1,6 @@
 class ContatoMailer < ApplicationMailer
   def contato(contato)
     @contato = contato
-    mail(to: 'santos.felipe13@gmail.com', subject: 'Novo contato')
+    mail(to: 'contato@hrdengenharia.com.br', subject: 'Novo contato pelo site')
   end
 end
